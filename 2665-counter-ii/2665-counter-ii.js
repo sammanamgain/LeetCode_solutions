1431127cf3c91282781b601016662b2f5816992e
Samman Amgain <amgain02@gmail.com>
2023-12-31 20:49:05 +0545

    Time: 124 ms (5.23%), Space: 43.6 MB (99.49%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    const re=init;
+    return {
+        increment:function()
+        {
+            return ++init;
+        },
+        decrement:function()
+        {
+            return --init;
+        },
+        reset:function()
+        {
+            init=re;
+            return init
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
