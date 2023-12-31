1beb6108fae79ccabcb28597f7ac3b2016c135ce
Samman Amgain <amgain02@gmail.com>
2023-12-31 20:49:57 +0545

    Time: 86 ms (5.35%), Space: 44.4 MB (5.67%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let result=init;
+    for(let i of nums){
+        result=fn(result,i)
+    }
+    return result 
+};
