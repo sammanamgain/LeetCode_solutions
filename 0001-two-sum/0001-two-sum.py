7616fc6c44d6d295050533257cd33334ddb4a695
Samman Amgain <amgain02@gmail.com>
2024-04-04 11:32:36 +0545

    Time: 67 ms (44.37%), Space: 17.4 MB (84.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,25 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap={}
-        for index,value in enumerate(nums):
-            print(index,value)
-            remaining=target-value
-            if remaining in hashmap:
-                firstindex=hashmap[remaining]
-                return [index,firstindex]
-            else:
-                hashmap[value]=index
-            
+        i=0
+        j=len(nums)-1
+        copy=list(nums)
+        nums.sort()
+        while(i<len(nums) and j<len(nums)):
+          
+        
+            sum=nums[i]+nums[j]
+
+            if(sum==target):
+                temp=[]
+
+                for k in range(len(copy)):
+                    if (copy[k]==nums[i] or copy[k]==nums[j]):
+                       
+                        temp.append(k)
+
+                return temp
+            if(sum>target):
+                j-=1
+            if(sum<target):
+                i+=1
         
