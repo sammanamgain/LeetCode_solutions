28b5296712fcdac963fbf320663a1954d3f4f71e
Samman Amgain <amgain02@gmail.com>
2024-04-10 17:14:41 +0545

    Time: 1891 ms (5.13%), Space: 23.7 MB (51.82%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -9,12 +9,17 @@ class Solution:
             if nums[r]%2!=0:
                 odd+=1
                 firstodd.append(r)
-            
-            while(odd>k):
-                if nums[l]%2!=0:
-                    odd-=1
-                    firstodd=firstodd[1:]
-                l+=1
+            # below code also work but i making more optimal , as we donot need to traverse again
+            # while(odd>k):
+            #     if nums[l]%2!=0:
+            #         odd-=1
+            #         firstodd=firstodd[1:]
+            #     l+=1
+            # no need to transverse one by one
+            if odd>k:
+                l=firstodd[0]+1
+                odd-=1
+                firstodd=firstodd[1:]
             if odd==k:
                 count+=1
                 count+=firstodd[0]-l
