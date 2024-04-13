45d5bc9240778f4a58b62ab1b57d5dbbcb3f1bfb
Samman Amgain <amgain02@gmail.com>
2024-04-13 22:06:41 +0545

    Time: 44 ms (25.8%), Space: 16.8 MB (7.63%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # we will start from filling the array from backwards
+        length=len(nums1)-1
+        l=m-1
+        r=n-1
+        while(l>=0 and r>=0):
+            print(nums1[l],nums2[r])
+            if nums1[l]>=nums2[r]:
+                nums1[length]=nums1[l]
+                l-=1
+                length-=1
+            else:
+                nums1[length]=nums2[r]
+                r-=1
+                length-=1
+        while(r>=0):
+            nums1[r]=nums2[r]
+            r-=1
+
+        
+        
+
+        
