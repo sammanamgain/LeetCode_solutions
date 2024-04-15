b85c263a95796fd5e25cd72a738c6fbd3070e04e
Samman Amgain <amgain02@gmail.com>
2024-04-15 17:52:35 +0545

    Time: 45 ms (38.73%), Space: 16.7 MB (59.72%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        hashmap = {}
+        hashmap1={}
+        l = 0
+        r = 0
+        while l < len(s) and r < len(t):
+            if s[l] not in hashmap:
+
+                hashmap[s[l]] = t[r]
+              
+            else:
+                if hashmap[s[l]] == t[r]:
+                    pass
+                   
+                else:
+                    return False
+            if t[r] not in hashmap1:
+
+                hashmap1[t[r]] = s[l]
+                l += 1
+                r += 1
+            else:
+                if hashmap1[t[r]] == s[l]:
+                    l += 1
+                    r += 1
+                else:
+                    return False
+        return True
+            
