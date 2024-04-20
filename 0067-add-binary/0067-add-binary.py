3028c0ce26e81c1b6c50ddd5762866c0b3a00130
Samman Amgain <amgain02@gmail.com>
2024-04-20 18:10:59 +0545

    Time: 41 ms (32.53%), Space: 16.6 MB (70.06%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -3,6 +3,8 @@ class Solution:
         result=""
         carry=0
         ML=max(len(b),len(a))
+
+        #making same length
         if ML>len(b):
             diff=ML-len(b)
             for i in range(diff):
@@ -13,22 +15,21 @@ class Solution:
             for i in range(diff):
                 a="0"+a
          
-        print(a,b)
-        print(ML)
-        print("in the loopo")
+        # working on operation (adding)
+  
         for i in range(ML-1,-1,-1):
             r=int(a[i])+int(b[i])+carry
             carry=0
-            print("priniting previous r",r)
+        
             if r==2:
                 carry=1
                 r=0
             if r==3:
                 carry=1
                 r=1
-            print("printing r",r)
+           
             result=str(r)+result
-            print("result",result)
+         
         if carry==1:
             result=str(carry)+result
         
