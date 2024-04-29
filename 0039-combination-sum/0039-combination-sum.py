b72138b96296116bf9ce1856caba37652c6d40c9
Samman Amgain <amgain02@gmail.com>
2024-04-29 15:28:41 +0545

    Time: 54 ms (52.3%), Space: 16.5 MB (85.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,31 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-
-        self.answer=[] # 2d list
-        self.calculatesum(candidates,[],i=0,sum=target)
+        self.answer = []
+        self.calculatesum(candidates, [], 0, target)
         return self.answer
 
-    def calculatesum(self,candidates,temp,i,sum):
-        print(temp)
-        print(i)
-        print(sum)
-
-        #best case
-        if sum==0:
-            print(temp)
-            self.answer.append(temp[::])
-            return 
-        
-        if i==len(candidates):
-            return 
-        if  sum<candidates[i]:
+    def calculatesum(self, candidates, temp, i, sum):
+        if sum < 0:
+            return
+        elif sum == 0:
+            self.answer.append(temp[:])
+            return
+        elif i == len(candidates):
             return
 
-        #lets take i
         temp.append(candidates[i])
-        sum-=candidates[i]
-        self.calculatesum(candidates,temp,i,sum)
+        self.calculatesum(candidates, temp, i, sum - candidates[i])
         temp.pop()
-        sum+=candidates[i]
-        # lets not take i
-        self.calculatesum(candidates,temp,i+1,sum)
+        self.calculatesum(candidates, temp, i + 1, sum)
