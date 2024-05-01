6f46be636e84c0633eae7613b93ac69d3c5419f7
Samman Amgain <amgain02@gmail.com>
2024-05-01 20:35:29 +0545

    Time: 32 ms (85.31%), Space: 16.7 MB (10.99%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -4,43 +4,22 @@ class Solution:
         path = path + '/'
         l = 0
         r = l + 1
-        flag = False
-
+        
         while r < len(path):
-            while path[r] != '/':
-                if (path[r] == '.' and path[r + 1] == '.' and path[r + 2] == '.'):
-                    r = r + 3
-                elif (path[r] == '.' and path[r + 1] == '.' and path[r + 2] == '/' and path[r - 1] == '/'):
-                    flag = True
-                    r += 1
-                    l = r + 2
-                    if dircollection:  # Check if the stack is not empty
+            if path[r] == '/':
+                if path[l + 1:r] == '..':
+                    if dircollection:
                         dircollection.pop()
-                else:
-                    r += 1
-
-            if flag == True:
-                flag = False
-            else:
-                if path[l + 1:r] != '.':
+                elif path[l + 1:r] != '.' and path[l + 1:r] != '':
                     dircollection.append(path[l + 1:r])
-
-            if r != len(path) - 1:
-                if path[r] == path[r + 1]:
-                    r += 1
-            l = r
+                l = r
             r += 1
-
+        
         if not dircollection:
             return '/'
-
+        
         result = ''
         for i in dircollection:
-            if i != '.' and i != '':
-                result += ('/' + i)
-
-        if result and result[-1] == '/':
-            return result[:-1]
-        elif result == "":
-            return '/'
+            result += '/' + i
+        
         return result
